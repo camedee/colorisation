a4813133a5892898a45ddd2c886ddde00938ca5c
Petru CUSNIR <petru.cusnir@depinfonancy.net>
2018-02-07 16:29:47 +0100

    colorflot par voisinage
    colorisation en prenant voisinage autour du pixel pointé par flot optique

--- colorflotParVoisinage/coord.m
+++ colorflotParVoisinage/coord.m
@@ -10,5 +10,6 @@ function [F1,F2]= coord( Imagetorec, flow)
 % Mouvement absolu à partir du relatif (flow). 
 F1 = nearest(max(min(flow(:,:,2)+X,y_max),1));
 F2 = nearest(max(min(flow(:,:,1)+Y,x_max),1));
+w = 42
 
 end
